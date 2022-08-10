003e016eebab0379dcb3091a260e32a7d97ecc68
Marcuschan2004 <110647103+Marcuschan2004@users.noreply.github.com>
2022-08-10 17:24:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5c66b16a91msh73202644311eb98p12909fjsnde305679b6a5',
+		'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
+	}
+};
 
+fetch('https://numbersapi.p.rapidapi.com/42/trivia?fragment=true&notfound=floor&json=true', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));

054ebcea4ca8c8fb63bc87ce55a6e0b34ffb6b3b
Marcuschan2004 <110647103+Marcuschan2004@users.noreply.github.com>
2022-08-10 18:55:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+let enter_btn = document.querySelector(".btn");
+let card = document.querySelector(".card");
 
+enter_btn.addEventListener("click", getFacts);
+
+function getFacts() {
+    let input = document.querySelector(".input").value;
+    let card_title = document.querySelector(".card_title");
+    let card_text = document.querySelector(".card-text");
+
+    fetch("http://numbersapi.com/5")
+    .then(response => response.text())
+    .then((data) => {
+        console.log(data);
+    });
+}

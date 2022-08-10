a8f1f6dee31dccd15683fc00855d07d5cd59a9e9
Marcuschan2004 <110647103+Marcuschan2004@users.noreply.github.com>
2022-08-10 17:20:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5c66b16a91msh73202644311eb98p12909fjsnde305679b6a5',
-		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-	}
-};
 
-fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));

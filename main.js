73a561bceede8c2c3116c6ac3ad32a537a6e1210
Marcuschan2004 <110647103+Marcuschan2004@users.noreply.github.com>
2022-08-10 17:02:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://quotes15.p.rapidapi.com/quotes/random/",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "5c66b16a91msh73202644311eb98p12909fjsnde305679b6a5",
-		"X-RapidAPI-Host": "quotes15.p.rapidapi.com"
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5c66b16a91msh73202644311eb98p12909fjsnde305679b6a5',
+		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
 	}
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));

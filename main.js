bc5edaba83387dcf312fb1cc09a4a9ba01f73484
Marcuschan2004 <110647103+Marcuschan2004@users.noreply.github.com>
2022-08-10 21:14:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,18 @@ function getFacts() {
     let card_title = document.querySelector(".card_title");
     let card_text = document.querySelector(".card-text");
 
-    if (input !=="") {
-        card.classList.remove("d-none");
+    const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5c66b16a91msh73202644311eb98p12909fjsnde305679b6a5',
+		'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
+	}
+};
 
-        fetch('http://numbersapi.com/${input}')
-    .then((response) => response.text())
-    .then((data) => {
+fetch('https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
         card_title.innerHTML = input;
         card_text.innerHTML = data;
     })

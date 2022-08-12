9843a65627f664b1f1b6b8e888c636e7a4f27d51
Marcuschan2004 <110647103+Marcuschan2004@users.noreply.github.com>
2022-08-11 21:07:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,40 +3,71 @@ let card = document.querySelector(".card");
 
 enter_btn.addEventListener("click", getFacts);
 
+let facts = []
+let timer = null
+
 function getFacts() {
+    if(timer != null)
+        clearTimeout(timer)
+
     let input = document.querySelector(".input").value;
-    let card_title = document.querySelector(".card_title");
     let card_text = document.querySelector(".card-text");
 
-   const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5c66b16a91msh73202644311eb98p12909fjsnde305679b6a5',
-		'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com'
-	}
-};
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '5c66b16a91msh73202644311eb98p12909fjsnde305679b6a5',
+            'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        referrerPolicy: 'no-referrer'
+    };
 
-fetch('https://numbersapi.p.rapidapi.com/5/trivia?fragment=true&notfound=floor&json=true', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-        card_title.innerHTML = input;
-        card_text.innerHTML = data;
-    }
+    fetch('https://numbersapi.p.rapidapi.com/' + input + '/trivia?fragment=true&notfound=floor&json=true', options)
+        .then((response) => {
+            if (!response.ok) {
+              throw new Error('Network response was not OK');
+            }
+            return response.json();
+          })
+        .then((response) => {
+            if(!facts.includes(response['text'])){
+                card_text.insertAdjacentHTML('beforeend','<ul><li>' + input + ': ' + response['text'] + '</li></ul>')
+                facts.push(response['text'])
+            }
+            else{
+                timer = setTimeout(getFacts, 1000);
+            }
+        })
+        .catch(err => console.error(err));
+}
 
 let copy_btn = document.querySelector(".copy-btn");
-copy_btn.addEventListener("click", () => {
-    copyText();
-})
+copy_btn.addEventListener("click",copyText)
+
+function gen_text(facts_array) {
+    let facts_string = ""
+    for (let i = 0; i < facts_array.length; i++){
+        if(i == facts_array.length-1){
+            facts_string += facts_array[i]
+        }
+        else{
+            facts_string += facts_array[i] + ", "
+        }
+    }
+    return facts_string
+}
 
 function copyText() {
-    const textarea = document.createElement("textarea");
-    let card_text = document.querySelector(".card-text").innerHTML;
+    if(timer != null)
+        clearTimeout(timer)
 
-    textarea.value = card_text;
-    document.body.append(textarea)
-    textarea.select()
-    document.execCommand("copy")
-    textarea.remove()
-    alert("Fact has been copied!");
+    let card_text = document.querySelector(".card-text");
+    card_text.innerHTML = ""
+    console.log(gen_text(facts))
+    navigator.clipboard.writeText(gen_text(facts)).then(() => {
+        alert("Fact has been copied!");
+    });
+    facts = []
 }
